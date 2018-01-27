3ddf27d18e8a4a14c524712b2a2f79a4594b353a
Simon Wrede <simwr872@student.liu.se>
2018-01-27 13:25:18 +0100

    Commented non-needed libraries in lua.

--- luascape/luascape/loslib.c
+++ luascape/luascape/loslib.c
@@ -385,14 +385,14 @@ static const luaL_Reg syslib[] = {
   {"clock",     os_clock},
   {"date",      os_date},
   {"difftime",  os_difftime},
-  {"execute",   os_execute},
+//{"execute",   os_execute},
   {"exit",      os_exit},
-  {"getenv",    os_getenv},
-  {"remove",    os_remove},
-  {"rename",    os_rename},
-  {"setlocale", os_setlocale},
+//{"getenv",    os_getenv},
+//{"remove",    os_remove},
+//{"rename",    os_rename},
+//{"setlocale", os_setlocale},
   {"time",      os_time},
-  {"tmpname",   os_tmpname},
+//{"tmpname",   os_tmpname},
   {NULL, NULL}
 };
 
